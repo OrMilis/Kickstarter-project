79da2c813a56b996333aa95df2054904afa57e2f
Or Milis <ormilism@gmail.com>
2018-07-17 18:22:52 +0300

    Create backer-project table fix.

--- migrations/20180717170323_create_backer_project_dictionary.js
+++ migrations/20180717170323_create_backer_project_dictionary.js
@@ -2,17 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projectInvest', function (table){
     table.integer('project_id').unsigned()
-    table.foreign('project_id').references('projects.user_id')
+    table.foreign('project_id').references('projects.id')
     table.integer('user_id').unsigned()
     table.foreign('user_id').references('users.id')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
   })
-=======
-  }
->>>>>>> b8ddceaf7317ecc2b27ae6a360992914f7bd577b
->>>>>>> c5c4937d80ed9cae7edca0dae9e03e9461f94e1a
 };
 
 exports.down = function(knex, Promise) {
