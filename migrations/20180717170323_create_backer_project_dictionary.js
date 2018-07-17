fdc43dc82b087be2f4a3d890137d46ac7dad6c64
Ran <ran.ben.amar77@gmail.com>
2018-07-17 17:25:11 +0300

    add projects - user table

---
+++ migrations/20180717170323_create_backer_project_dictionary.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projectInvest', function (table){
+    table.integer('project_id').unsigned()
+    table.foreign('project_id').references('projects.user_id')
+    table.integer('user_id').unsigned()
+    table.foreign('user_id').references('users.id')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projectInvest')
+
+};
