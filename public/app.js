923da1e456259090405681bb0c5ead5e3c574335
Or Milis <ormilism@gmail.com>
2018-07-29 18:23:12 +0300

    God Merge

--- public/app.js
+++ public/app.js
@@ -165,14 +165,11 @@ function getProjectPage(user_name, project_name) {
       }
     )
     .then(data => {
-<<<<<<< HEAD
       document.querySelector('.Body').innerHTML = data;
-=======
       console.log(data);
       document
         .querySelector('.Body')
         .innerHTML = data;
->>>>>>> 6f3265435817b7c422c2f62e3a474f463f7e868d
     })
     .catch(error => {
       console.log('Error is', error);
